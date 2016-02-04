ea8550c0812958e19d9856f0f29ae9d4073d22dc
Mat Bishop <matthew.g.bishop@accenture.com>
2016-02-04 20:46:52 +0000

    update to template

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,5 +7,5 @@
 @endsection
 
 @section('content')
-    <p>The wedding of <span class="title">Mat Bishop</span> and <span class="title">Nadia Bettout</span> will be held on <time>17th June, 2017</time> at Houchins Barn, Essex.</p>
+    <p>The wedding of <span class="title">Nadia Bettout</span> and <span class="title">Mat Bishop</span> will be held on <time>17th June, 2017</time> at <a href="http://www.houchins.co.uk/">Houchins Farm, Essex.</a></p>
 @endsection
