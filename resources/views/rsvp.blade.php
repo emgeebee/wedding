ef2e95802e0cfe53e17d8698d982b0f186cc5e96
Mat Bishop <matthew.g.bishop@accenture.com>
2016-10-08 12:50:30 +0100

    updated email

--- resources/views/rsvp.blade.php
+++ resources/views/rsvp.blade.php
@@ -8,6 +8,6 @@
 
 @section('content')
     <p>
-        Please RSVP to us <a href="mailto:mat4nad@gmail.com">via email</a> no later than 31st April 2017
+        Please RSVP to us <a href="mailto:matandnads@emgeebee.com">via email</a> no later than 31st April 2017
     </p>
 @endsection
